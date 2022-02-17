71a15878d48ac020998851b8bf234d524ce3ccb5
maneeshaek <maneesha.falcon@gmail.com>
2022-02-17 06:54:36 +0530

    Product list and search

---
+++ js/script.js
@@ -0,0 +1,91 @@
+const products =[
+    {
+        id: 1,
+        name: 'prd -1',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 2,
+        name: 'prd -2',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 3,
+        name: 'prd -3',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 4,
+        name: 'prd -4',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 5,
+        name: 'prd -5',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 6,
+        name: 'prd -6',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 7,
+        name: 'prd -7',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 8,
+        name: 'prd -8',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 9,
+        name: 'prd -9',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 10,
+        name: 'prd -10',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+    {
+        id: 10,
+        name: 'srd -10',
+        price: 12,
+        description: "akside cjencioe",
+        quantity: 45,
+        image: 'images/pro_1.jpg'
+    },
+
+]
